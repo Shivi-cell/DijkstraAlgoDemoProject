166e33144183697cd27b1423ea0da5f6dd9329e1
Shivam Chaubey <shivamchaubey005@gmail.com>
2022-12-23 16:13:51 +0530

    Initial commit

---
+++ src/Graph.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class Graph {
+    Set<Node> nodes = new LinkedHashSet<>();
+
+    public void addNode(Node nodeA) {
+        nodes.add(nodeA);
+    }
+
+    public Set<Node> getNodes() {
+        return nodes;
+    }
+
+    public void setNodes(Set<Node> nodes) {
+        this.nodes = nodes;
+    }
+
+    public void printGraph()
+    {
+        System.out.println("Printing graph info..");
+        for (Node n : nodes)
+        {
+            System.out.println("Node "+n.getName());
+            List<Node> shortestPath = n.getShortestPath();
+            System.out.print("Shortest path -> ");
+//            int count = 0;
+            for (Node n1 : shortestPath)
+            {
+//                count += n1.getDistance();
+                System.out.print(n1.getName()+" ");
+            }
+//            System.out.print(count);
+            System.out.println("");
+            for (Map.Entry<Node, Integer> adjNodes : n.getAdjacentNodes().entrySet())
+            {
+                System.out.println("Destination -> "+adjNodes.getKey().getName()+" with weight "+adjNodes.getValue());
+
+            }
+        }
+    }
+}
